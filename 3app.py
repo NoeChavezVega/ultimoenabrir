744fd51f1b32037f78bcf3654ac9fa6393d506b9
NoeChavezVega <a377347@uach.mx>
2025-11-18 23:00:07 -0600

    Update 3app.py

--- 3app.py
+++ 3app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 st.title("🌱 EcoAprende de Energías🔌")
-progreso = {"Solar": {"completado": False, "puntaje": 0},
+progreso = {"Solar⛏": {"completado": False, "puntaje": 0},
     "Eolica": {"completado": False, "puntaje": 0},
     "Hidraulica": {"completado": False, "puntaje": 0},
     "Biomasa": {"completado": False, "puntaje": 0},}
@@ -33,7 +33,7 @@ def mostrar_preguntas(preguntas, juego):
         st.balloons()
         st.session_state["pantalla"] = "dashboard"
 
-preguntas_Solar = [
+preguntas_Solar⛏ = [
     {"pregunta":"¿Qué tipo de tecnología utiliza la energía solar?",
      "opciones":["pirolisis y carbonización","multiplicadora","mecánica","fotovoltaica"],
      "correcta":"fotovoltaica"},
